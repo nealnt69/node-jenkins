402d609a1610e45f550ce9af66324c693c59746b
namnt <nam.nguyentrong@jmango360.com>
2021-06-18 13:31:50 +0700

    Qwe

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 http
   .createServer(function (req, res) {
-    res.write("Hello World!!!");
+    res.write("Hello World!");
     res.end();
   })
   .listen(8000, "0.0.0.0");
